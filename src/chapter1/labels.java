cd7459bbe42c8462f6cd830901da00706c39b4cc
Hammad_Jilani <hjilani15@gmail.com>
2024-03-09 21:03:34 +0500

    New file

---
+++ src/chapter1/labels.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+import javax.swing.*;
+
+public class labels {
+    public static void main(String[] args) {
+        JLabel label = new JLabel();
+        label.setText("My name is Hammad Ali Jilani");
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+}
